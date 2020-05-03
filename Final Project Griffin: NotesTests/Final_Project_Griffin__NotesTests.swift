ac71ffd5cb957cba3b8295b789e1b562cfec08a1
Kai Marshall <k.marshall@setonhill.edu>
2020-05-02 20:13:09 -0400

    Initial Commit

---
+++ Final Project Griffin: NotesTests/Final_Project_Griffin__NotesTests.swift
@@ -0,0 +1,34 @@
+//
+//  Final_Project_Griffin__NotesTests.swift
+//  Final Project Griffin: NotesTests
+//
+//  Created by Kai Marshall on 5/2/20.
+//  Copyright © 2020 Kai Marshall. All rights reserved.
+//
+
+import XCTest
+@testable import Final_Project_Griffin__Notes
+
+class Final_Project_Griffin__NotesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
